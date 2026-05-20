3a135e34b62b64de54a91e1f2283494bb74092be
Axcelion017 <michaelnewton1708@gmail.com>
2026-05-20 16:20:06 +0700

    JsonHelper and gradle

--- app/src/main/java/tubespbo/helper/JsonHelper.java
+++ app/src/main/java/tubespbo/helper/JsonHelper.java
@@ -1,5 +1,15 @@
 package tubespbo.helper;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 public class JsonHelper {
-    
+    private static final Gson gson = new GsonBuilder()
+            .setPrettyPrinting() 
+            .serializeNulls() 
+            .create();
+
+    public static Gson getGson() {
+        return gson;
+    }
 }
