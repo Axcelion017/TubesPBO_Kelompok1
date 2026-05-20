5ec5c94179bda0d90d85dc3cc8d1a2509ee6e595
Arif Mufti Tharsa <105224041@student.universitaspertamina.ac.id>
2026-05-21 01:44:27 +0700

    Memisahkan dashboard dari fitur inventaris

--- app/src/main/java/tubespbo/presentation/Main.java
+++ app/src/main/java/tubespbo/presentation/Main.java
@@ -1,48 +1,41 @@
 package tubespbo.presentation;
 
-import tubespbo.domain.Role;
 import tubespbo.domain.User;
-import tubespbo.repository.KendaraanRepository;
 import tubespbo.repository.UserRepository;
 import tubespbo.service.AuthService;
-import tubespbo.service.InventarisService;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main{
     public static void main(String[] args) {
+        // 1. Inisialisasi Repository dan Service
         UserRepository userRepo = new UserRepository();
         AuthService authService = new AuthService(userRepo);
-        KendaraanRepository kendaraanRepository = new KendaraanRepository();
-        InventarisService inventarisService = new InventarisService(kendaraanRepository);
-
+        
         Scanner scanner = new Scanner(System.in);
         User userLogin = null;
 
-        System.out.println("========================================");
-        System.out.println("     SELAMAT DATANG DI TUBES PBO");
-        System.out.println("========================================");
+        System.out.println("=== SISTEM TESTING AUTHENTICATION ===");
+        System.out.println("Database default otomatis terbuat di data/user.json");
         System.out.println("Akun default: admin/admin123, staff/staff123, owner/owner123");
+        System.out.println("=====================================");
 
+        // 2. Loop login selama belum sukses dan sistem belum dikunci
         while (userLogin == null && !authService.isTerkunci()) {
             System.out.print("\nMasukkan Username: ");
             String username = scanner.nextLine();
-
+            
             System.out.print("Masukkan Password: ");
             String password = scanner.nextLine();
 
+            // Panggil service auth yang kamu buat tadi
             userLogin = authService.login(username, password);
         }
 
+        // 3. Cek hasil akhir setelah keluar dari loop
         if (userLogin != null) {
-            System.out.println("\n[SUKSES] Login berhasil sebagai " + userLogin.getRole() + ".");
-
-            MenuHandler menuHandler = new MenuHandler(scanner, inventarisService);
-            if (userLogin.getRole() == Role.ADMIN) {
-                menuHandler.tampilkanDashboardAdmin(userLogin.getUsername());
-            } else {
-                System.out.println("Dashboard " + userLogin.getRole() + " belum dikerjakan di Epic 2.");
-            }
+            System.out.println("\n[SUKSES] Selamat Datang, " + userLogin.getUsername() + "!");
+            System.out.println("Hak Akses Anda (Role): " + userLogin.getRole());
         } else {
             System.out.println("\n[KUNCI] Aplikasi dihentikan karena terlalu banyak kesalahan.");
         }
