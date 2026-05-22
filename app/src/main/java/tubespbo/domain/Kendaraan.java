9ae6429a120dd9110a369893a27c982e17422734
Axcelion017 <michaelnewton1708@gmail.com>
2026-05-22 21:43:47 +0700

    Vehicle Class and Subclasses

--- app/src/main/java/tubespbo/domain/Kendaraan.java
+++ app/src/main/java/tubespbo/domain/Kendaraan.java
@@ -1,5 +1,41 @@
 package tubespbo.domain;
 
 public abstract class Kendaraan {
-    
+    private String platNomor, merk;
+    private int hargaSewaPerHari;
+    private StatusKendaraan status;
+
+    public Kendaraan(String platNomor, String merk, int hargaSewaPerHari) {
+        this.platNomor = platNomor;
+        this.merk = merk;
+        this.hargaSewaPerHari = hargaSewaPerHari;
+        this.status = StatusKendaraan.TERSEDIA;
+    }
+
+    public String getPlatNomor() {
+        return platNomor;
+    }
+
+    public String getMerk() {
+        return merk;
+    }
+
+    public int getHargaSewaPerHari() {
+        return hargaSewaPerHari;
+    }
+
+    public StatusKendaraan getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusKendaraan status) {
+        this.status = status;
+    }
+
+    public abstract String getJenis();
+
+    public abstract String getInfoTambahan();
+
+    public abstract double hitungDendaKeterlambatan(int hariTerlambat);
+
 }
--- app/src/main/java/tubespbo/domain/Mobil.java
+++ app/src/main/java/tubespbo/domain/Mobil.java
@@ -1,5 +1,29 @@
 package tubespbo.domain;
 
 public class Mobil extends Kendaraan {
-    
+    private int jumlahPintu;
+
+    public Mobil(String platNomor, String merk, int hargaSewaPerHari, int jumlahPintu) {
+        super(platNomor, merk, hargaSewaPerHari);
+        this.jumlahPintu = jumlahPintu;
+    }
+
+    @Override
+    public double hitungDendaKeterlambatan(int hariTerlambat){
+        return hariTerlambat * (getHargaSewaPerHari() * 0.10);
+    }
+
+    @Override
+    public String getJenis() {
+        return "Mobil";
+    }
+
+    @Override
+    public String getInfoTambahan() {
+        return "Jumlah Pintu: " + jumlahPintu;
+    }
+
+    public int getJumlahPintu() {
+        return jumlahPintu;
+    }
 }
