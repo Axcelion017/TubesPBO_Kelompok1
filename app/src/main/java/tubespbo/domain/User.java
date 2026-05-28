42b0bb72c07afb511740943911d85fc4284520ef
Razqa <razqaazaki55@gmail.com>
2026-05-29 00:39:31 +0700

    feat: feature/user-database

--- app/src/main/java/tubespbo/domain/User.java
+++ app/src/main/java/tubespbo/domain/User.java
@@ -1,7 +1,25 @@
 package tubespbo.domain;
 
 public class User {
-    ADMIN,
-    STAFF,
-    OWNER
+     private String username;
+    private String password;
+    private Role role;
+
+    public User(String username, String password, Role role) {
+        this.username = username;
+        this.password = password;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
 }
