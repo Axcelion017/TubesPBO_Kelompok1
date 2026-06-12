d65bfd6c654cf781325dfdb8505af2a3ea61b788
Axcelion017 <michaelnewton1708@gmail.com>
2026-06-12 22:54:07 +0700

    Add UI Login, Service, Handling, adn Exception of Login

--- app/src/main/java/tubespbo/presentation/Main.java
+++ app/src/main/java/tubespbo/presentation/Main.java
@@ -1,6 +1,32 @@
 package tubespbo.presentation;
 
+import java.util.Scanner;
+import tubespbo.domain.User;
+import tubespbo.repository.UserRepository;
+import tubespbo.service.AuthService;
+
 public class Main {
     public static void main(String[] args) {
+        // 1. COMPOSITION ROOT (Setup Dependency Injection)
+        // Di sinilah semua object dibuat dan dirakit sebelum aplikasi berjalan
+        Scanner scanner = new Scanner(System.in);
+        UserRepository userRepository = new UserRepository();
+
+        // Perhatikan tipe datanya adalah Interface LoginService (DIP)
+        AuthService authService = new AuthService(userRepository);
+
+        // 2. MENAMPILKAN LOGIN VIEW
+        // Urusan UI login diserahkan ke class LoginView (SRP)
+        MenuHandler loginView = new MenuHandler(authService, scanner);
+        User loggedInUser = loginView.tampilkanLayarLogin();
+
+        // 3. MASUK KE DASHBOARD (Nantinya dibuatkan DashboardView terpisah)
+        System.out.println("\n--- Masuk ke Dashboard " + loggedInUser.getRole() + " ---");
+
+        // switch (loggedInUser.getRole()) {
+        // case ADMIN: showMenuAdmin(); break;
+        // case STAFF: showMenuStaff(); break;
+        // case OWNER: showMenuOwner(); break;
+        // }
     }
 }
--- app/src/main/java/tubespbo/service/AuthService.java
+++ app/src/main/java/tubespbo/service/AuthService.java
@@ -1,5 +1,36 @@
 package tubespbo.service;
 
-public class AuthService {
+import java.util.Optional;
+import tubespbo.domain.User;
+import tubespbo.exception.LoginGagalException;
+import tubespbo.repository.UserRepository;
 
+interface LoginService {
+    User login(String username, String password);
+}
+
+// SOLID - Dependency Inversion Principle (DIP):
+// Class bergantung pada abstraction (interface LoginService dan UserRepository)
+public class AuthService implements LoginService {
+
+    private final UserRepository userRepository;
+
+    // Dependency Injection
+    public AuthService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User login(String username, String password) {
+        Optional<User> userOptional = userRepository.cariByUsername(username);
+
+        if (userOptional.isPresent()) {
+            User user = userOptional.get();
+            if (user.getPassword().equals(password)) {
+                return user;
+            }
+        }
+
+        throw new LoginGagalException("Login Gagal! Username atau password salah.");
+    }
 }
