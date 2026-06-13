20f9db4e83eb4edea74eb67ec5625fe98408d03b
danu44-1 <danudimas042@gmail.com>
2026-06-13 15:30:39 +0700

    Add daftarPelanggan(), Constructor StaffMenuHandler

--- app/src/main/java/tubespbo/presentation/StaffMenuHandler.java
+++ app/src/main/java/tubespbo/presentation/StaffMenuHandler.java
@@ -1,5 +1,19 @@
 package tubespbo.presentation;
 
+import java.util.Scanner;
+
+import tubespbo.service.PelangganService;
+import tubespbo.service.RentalService;
+
 public class StaffMenuHandler {
+    private Scanner scanner;
+    private PelangganService pelangganService;
+    private RentalService rentalService;
+    
+    public StaffMenuHandler(Scanner scanner, PelangganService pelangganService, RentalService rentalService) {
+        this.scanner = scanner;
+        this.pelangganService = pelangganService;
+        this.rentalService = rentalService;
+    }
     
 }
