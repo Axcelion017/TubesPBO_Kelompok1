3bb7302d55190a497afc1d85dac04e3db439aa0c
Razqa <razqaazaki55@gmail.com>
2026-06-18 16:56:55 +0700

    feat: add duplicate plate number exception

--- app/src/main/java/tubespbo/exception/PlatNomorDuplikatException.java
+++
@@ -1,7 +0,0 @@
-package tubespbo.exception;
-
-public class PlatNomorDuplikatException extends Exception {
-    public PlatNomorDuplikatException(String platNomor) {
-        super("Plat nomor " + platNomor + " sudah terdaftar di sistem.");
-    }
-}
