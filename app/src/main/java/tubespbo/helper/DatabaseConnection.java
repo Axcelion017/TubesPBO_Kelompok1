6003cf3066a97417dbf40cabeebea8a18453e829
danu44-1 <danudimas042@gmail.com>
2026-06-18 15:29:22 +0700

    add daftarPelanggan, Constructor StaffMenuHandler (2)

--- app/src/main/java/tubespbo/helper/DatabaseConnection.java
+++ app/src/main/java/tubespbo/helper/DatabaseConnection.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DatabaseConnection {
-    private static final String URL = "jdbc:oracle:thin:@localhost:1521/XEPDB1";
+    private static final String URL = "jdbc:oracle:thin:@100.114.119.105:1521/XEPDB1";
     private static final String USERNAME = "TUBESPBO";
     private static final String PASSWORD = "TubesPBO19";
 
