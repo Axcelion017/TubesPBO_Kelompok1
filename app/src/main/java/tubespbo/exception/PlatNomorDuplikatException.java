f30a8227eeb5ff086b02fc72fd239c3a2441b41a
Razqa <razqaazaki55@gmail.com>
2026-06-20 09:32:20 +0700

    Update PlatNomorDuplikatException

---
+++ app/src/main/java/tubespbo/exception/PlatNomorDuplikatException.java
@@ -0,0 +1,7 @@
+package tubespbo.exception;
+
+public class PlatNomorDuplikatException extends Exception {
+    public PlatNomorDuplikatException(String platNomor){
+        super("Plat nomor" + platNomor + " sudah terdaftar di sistem.");
+    }
+}
